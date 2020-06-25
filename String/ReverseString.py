3a55e534e628ce5ad171432a3a196b979c3f7b6f
Chandresh Bhatt <33648618+BhattChandresh@users.noreply.github.com>
2020-06-25 18:49:50 +0530

    Update ReverseString.py

--- String/ReverseString.py
+++ String/ReverseString.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
-
-
 #Method-1
 str = 'Chandresh'
 def reverse(str): 
@@ -13,18 +10,13 @@ def reverse(str):
     return revstr
 
 reverse(str)
-        
-
-
-# In[7]:
-
 
 #Method-2
 rev = str[::-1]
 print("Revser String is = ",rev)
 
 
-# In[ ]:
+
 
 
 
