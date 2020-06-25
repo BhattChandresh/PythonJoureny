ae3d7541aea287962ac73d3f8fcf2182b1f48e86
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2020-06-25 18:39:20 +0530

    First commit - ReverseString.py

---
+++ String/ReverseString.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Method-1
+str = 'Chandresh'
+def reverse(str): 
+    revstr =''
+    for i in str:
+        revstr = i + strrev
+    return revstr
+
+reverse(str)
+        
+
+
+# In[7]:
+
+
+#Method-2
+rev = str[::-1]
+print("Revser String is = ",rev)
+
+
+# In[ ]:
+
+
+
+
